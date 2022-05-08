89b9ca92fba6eac14f5b9cb44cbd0b0ac7b9a185
Alexander Rodionov <a.rodionov@copybet.com>
2022-05-08 13:24:15 +0300

    #1: table name as option

--- pkg/db/sql.go
+++ pkg/db/sql.go
@@ -2,11 +2,13 @@ package db
 
 import "strings"
 
+const tableTemplate = "{{table}}"
+
 type (
 	sqlTemplate string
 	sqlTable    string
 )
 
 func (te sqlTemplate) sql(t sqlTable) string {
-	return strings.ReplaceAll(string(te), "{{table}}", string(t))
+	return strings.ReplaceAll(string(te), tableTemplate, string(t))
 }
